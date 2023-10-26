9ad2b25e1204217df36201f6f0725c6c461d6bbd
ilarburu1 <144702563+ilarburu1@users.noreply.github.com>
2023-10-26 14:52:16 +0000

    add

--- app.js
+++ app.js
@@ -10,22 +10,7 @@ app.use(express.static('public'))
 //formularioak tratatzeko middleware
 app.use(express.urlencoded({ extended: false }));
 
-app.get("/", function(req, res) {
-    res.send("Kaixo mundua!");
-});
 
-app.get('/bezeroa/:izena', function(req, res) {
-    res.send("Bezeroaren izena: " + req.params.izena);
-});
-
-app.get('/bezero', function(req, res) {
-    res.send(`Kaixo ${req.query.izena} ${req.query.abizena}`);
-});
-
-//Bezeroa POST
-app.post("/bezeroa",function(req,res){
-    res.send(`Kaixo ${req.body.izena} ${req.body.abizena}`);
-})
 
 app.listen( 3000, function() {
     console.log("Zerbitzaria 3000 portuan entzuten");
