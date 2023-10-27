269abedb561476253f2cd4a6be2303051d4754f9
ilarburu1 <144702563+ilarburu1@users.noreply.github.com>
2023-10-27 14:38:01 +0000

    b

--- app.js
+++ app.js
@@ -15,3 +15,10 @@ app.use(express.urlencoded({ extended: false }));
 app.listen( 3000, function() {
     console.log("Zerbitzaria 3000 portuan entzuten");
 })
+app.post("/bezeroa",function(req,res){
+    res.send(
+        "Izena: "+req.body.name+", Emaila:"+req.body.email+", Pasahitza: "+req.body.password+", Konfirmatu pasahitza: "+req.body.confirmPassword+", Generoa:"
+        +req.body.gender+", Data: "+req.body.date+", Helbidea: "+req.body.address+", Herria: "+req.body.city+", Posta kodea: "+req.body.zipCode+", Kondizioak: "+req.body.conditions+" Newsletter: "+req.body.newsletter
+
+    )
+})
